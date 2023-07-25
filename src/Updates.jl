f336cce214ef385c38c126163f2eb7961b41ef2d
Anjishnu Bose <39477277+Anjishnubose@users.noreply.github.com>
2023-07-25 18:10:47 +0530

    Update Updates.jl

--- src/Updates.jl
+++ src/Updates.jl
@@ -11,10 +11,10 @@ module Updates
 	```
 
 	Returns a dictionary containing the following
-    	- "VInNext" : simple mixing of VIn and VOut with weight (1-`alpha`) and `alpha` respectively. 
-    	- "VOutNext" : The function `F` evaluated at VInNExt, with fixed args `F_args` and kwargs `F_kwargs`.
-    	- "Delta" : The norm difference b/w the next input and output.
-    	- "kwargs" : the kwargs of this function itself, to be used by a subsequent function call in the next fixed point iteration. Since this is simple mixing, `alpha` is held constant.
+    	* "VInNext" : simple mixing of VIn and VOut with weight (1-`alpha`) and `alpha` respectively. 
+    	* "VOutNext" : The function `F` evaluated at VInNExt, with fixed args `F_args` and kwargs `F_kwargs`.
+    	* "Delta" : The norm difference b/w the next input and output.
+    	* "kwargs" : the kwargs of this function itself, to be used by a subsequent function call in the next fixed point iteration. Since this is simple mixing, `alpha` is held constant.
 
 	"""
     function SimpleMixing(VIn::N, VOut::N, F::T ; F_args::Tuple = (), F_kwargs::Dict = Dict() , alpha::Float64 = 0.5, _extra...) :: Dict   where {T<:Function, N<:Union{Number, Vector{<:Number}}}
